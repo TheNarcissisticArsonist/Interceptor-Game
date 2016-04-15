58ab5a3d3001bcd426ba96002a9a0e62a93623b3
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2016-04-15 10:14:43 -0400

    UI formatting.

--- script.js
+++ script.js
@@ -8,18 +8,16 @@
 // Non-Javascript Constants
 //----------------------------------------------------------------------------------------------------
 
-var userInterface = "<div id='gameScreen'>				\
-	<div id='svgCont'>									\
-		<svg id='gameArea'></svg>						\
-	</div>												\
-	<div id='uiBottomLeft'>								\
-		<div id='dummyControl1'></div>					\
-		<div id='dummyInfo1'></div>						\
-	</div>												\
-	<div id='uiBottomRight'>							\
-		<div id='dummyControl2'></div>					\
-		<div id='dummyInfo2'></div>						\
-	</div>												\
+var userInterface = "<div id='gameScreen'>\
+	<div id='svgCont'>\
+		<svg id='gameArea'></svg>\
+	</div>\
+	<div id='uiBottomLeft'>\
+		<div id='dummyInfo1Cont'>Info: <span id='dummyInfo1'>___</span></div>\
+	</div>\
+	<div id='uiBottomRight'>\
+		<div id='dummyInfo2Cont'>Info: <span id='dummyInfo2'>___</span></div>\
+	</div>\
 	</div>";
 
 //----------------------------------------------------------------------------------------------------
@@ -42,9 +40,25 @@ function clearPage() {
 	removeElementById("pageLoadText");
 	document.body.innerHTML += "<div id='wholeScreen'></div>";
 	htmlElements.wholeScreen = document.getElementById("wholeScreen");
+
+	htmlElements.wholeScreen.style.width = "100vw";
+	htmlElements.wholeScreen.style.height = "100vh";
 }
 function loadUI() {
 	htmlElements.wholeScreen.innerHTML = userInterface;
+	htmlElements.gameArea = document.getElementById("gameArea");
+
+	//Store ui info panels to htmlElements for access later
+	
+	gameScreen = document.getElementById("gameScreen");
+	uiBottomLeft = document.getElementById("uiBottomLeft");
+	uiBottomRight = document.getElementById("uiBottomRight");
+	dummyInfo1Cont = document.getElementById("dummyInfo1Cont");
+	dummyInfo2Cont = document.getElementById("dummyInfo2Cont");
+
+	gameScreen.style.width = "100%";
+	gameScreen.style.height = "100%";
+	gameScreen.style.backgroundColor = "#000000";
 }
 
 //----------------------------------------------------------------------------------------------------
