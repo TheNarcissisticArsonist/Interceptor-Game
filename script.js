8c054b44fd32d9d80ac78f5f5d41ec4532db4d20
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2016-04-15 16:20:15 -0400

    I am the KING OF CSS!!!!

--- script.js
+++ script.js
@@ -10,13 +10,35 @@
 
 var userInterface = "<div id='gameScreen'>\
 	<div id='svgCont'>\
-		<svg id='gameArea'></svg>\
+		<svg id='gameArea' width='0px' height='0px'></svg>\
 	</div>\
 	<div id='uiBottomLeft'>\
-		<div id='dummyInfo1Cont'>Info: <span id='dummyInfo1'>___</span></div>\
+		<div id='speedInfoCont'>Speed: <span id='speedInfo'>___</span> km/h</div>\
+		<br>\
+		<div id='boostBarInfoCont'>Boost: <div id='boostBar'></div></div>\
 	</div>\
 	<div id='uiBottomRight'>\
-		<div id='dummyInfo2Cont'>Info: <span id='dummyInfo2'>___</span></div>\
+		<div id='shieldsInfoCont'>Shields:\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+			<div class='shieldInfo'></div>\
+		</div>\
+		<br>\
+		<div id='healthInfoCont'>Health:\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+			<div class='healthInfo'></div>\
+		</div>\
 	</div>\
 	</div>";
 
@@ -24,7 +46,7 @@ var userInterface = "<div id='gameScreen'>\
 // Global Variables
 //----------------------------------------------------------------------------------------------------
 
-htmlElements = {};
+htmlElements = {}; //This object is populated later in the program, once the page is setup
 
 //----------------------------------------------------------------------------------------------------
 // Classes
@@ -50,15 +72,43 @@ function loadUI() {
 
 	//Store ui info panels to htmlElements for access later
 	
-	gameScreen = document.getElementById("gameScreen");
-	uiBottomLeft = document.getElementById("uiBottomLeft");
-	uiBottomRight = document.getElementById("uiBottomRight");
-	dummyInfo1Cont = document.getElementById("dummyInfo1Cont");
-	dummyInfo2Cont = document.getElementById("dummyInfo2Cont");
+	//Grab these for use in this function only
+	var gameScreen = document.getElementById("gameScreen");
+	var svgCont = document.getElementById("svgCont");
+	var svgArea = document.getElementById("gameArea");
+	var uiBottomLeft = document.getElementById("uiBottomLeft");
+	var uiBottomRight = document.getElementById("uiBottomRight");
+	var uiSpeedInfoCont = document.getElementById("speedInfoCont");
+	var uiBoostBarInfoCont = document.getElementById("boostBarInfoCont");
+	var uiBoostBar = document.getElementById("boostBar");
+	var uiShieldsInfoCont = document.getElementById("shieldsInfoCont");
+	var uiShieldArray = document.getElementsByClassName("shieldInfo");
+	var uiHealthInfoCont = document.getElementById("healthInfoCont");
+	var uiHealthArray = document.getElementsByClassName("healthInfo");
 
+	//Game screen and container formatting
 	gameScreen.style.width = "100%";
 	gameScreen.style.height = "100%";
-	gameScreen.style.backgroundColor = "#000000";
+	gameScreen.style.backgroundColor = "#87cefa"; //Sky blue
+
+	//SVG area formatting
+
+	svgArea.setAttribute("width", "100vw");
+	svgArea.setAttribute("height", "100vh");
+	svgArea.style.position = "fixed";
+	svgArea.style.left = "0px";
+	svgArea.style.top = "0px";
+
+	//Bottom left UI formatting
+	uiBottomLeft.style.position = "fixed";
+	uiBottomLeft.style.bottom = "0px";
+	uiBottomLeft.style.left = "0px";
+	uiBottomLeft.style.width = String(fractionWindowWidth(8))+"px";
+
+	//Bottom right UI formatting
+	uiBottomRight.style.position = "fixed";
+	uiBottomRight.style.bottom = "0px";
+	uiBottomRight.style.right = "0px";
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -80,6 +130,12 @@ function removeElement(element) {
 function removeElementById(elementID) {
 	var toRemove = document.getElementById(elementID);
 	removeElement(toRemove);
+}
+function fractionWindowWidth(denominator) {
+	return Number(window.getComputedStyle(gameScreen, null).getPropertyValue("width").slice(0, -2))/denominator;
+}
+function fractionWindowHeight(denominator) {
+	return Number(window.getComputedStyle(gameScreen, null).getPropertyValue("height").slice(0,-2))/denominator;
 }
 
 //----------------------------------------------------------------------------------------------------
